83639d9271ca5ba918356da770a9637a9d59f672
guclemen <guilhermeclementedealmeida@gmail.com>
2025-05-14 14:03:47 -0300

    fix: :bug: fix export change variable

--- minishell/src/builtins2.c
+++ minishell/src/builtins2.c
@@ -6,7 +6,7 @@
 /*   By: guclemen <guclemen@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/02 16:17:20 by guclemen          #+#    #+#             */
-/*   Updated: 2025/05/13 16:30:32 by guclemen         ###   ########.fr       */
+/*   Updated: 2025/05/14 14:01:23 by guclemen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,32 @@ static int	is_valid_export(char *str)
 		return (0);
 	return (1);
 }
-char *ft_no_spaces(const char *str)
+static char	*ft_no_spaces(char *str)
 {
 	while (*str == ' ' || *str == '\t')
 		str++;
 	return ft_strdup(str);
+}
+static int	ft_change_value(char **env, char *new_var)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (env[i])
+	{
+		j = 0;
+		while (new_var[j] && env[i][j] == new_var[j] && new_var[j] != '=' && env[i][j] != '=')
+			j++;
+		if (new_var[j] == '=' && env[i][j] == '=')
+		{
+			free(env[i]);
+			env[i] = ft_no_spaces(new_var);
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
 char	**ft_export(char **env, char *new_var)
@@ -42,9 +63,10 @@ char	**ft_export(char **env, char *new_var)
 
 	if (!is_valid_export(new_var))
 		return (env);
+	if (ft_change_value(env, new_var))
+		return (env);
 	count = count_env(env) + 1;
 	new_env = alloc_env(count);
-	// devo retornar a env ou seguir a vida
 	if (!new_env)
 		return (env);
 	no_spaces = ft_no_spaces(new_var);
@@ -62,14 +84,13 @@ char	**ft_unset(char **env, char *to_remove)
 		return (env);
 	count = count_env(env) - 1;
 	char **new_env = alloc_env(count);
-	// devo retornar a env ou seguir a vida
 	if (!new_env)
 		return (env);
 	copy_env_skip(env, new_env, to_remove, NULL);
 	free_env(env);
 	return (new_env);
 }
-/*
+
 int main(void) {
     char *test_env[] = {
         "USER=guest",
@@ -95,6 +116,7 @@ int main(void) {
     env = ft_export(env, "SINGLECHAR=A");
     env = ft_export(env, "    KEY=   spacedvalue");
     env = ft_export(env, "MIXED=   content with spaces   ");
+    env = ft_export(env, "TEST2=vasco");
 
     printf("After export tests (%d entries):\n", count_env(env));
     for (int i = 0; env[i]; i++)
@@ -112,4 +134,3 @@ int main(void) {
     free_env(env);
     return 0;
 }
-*/
